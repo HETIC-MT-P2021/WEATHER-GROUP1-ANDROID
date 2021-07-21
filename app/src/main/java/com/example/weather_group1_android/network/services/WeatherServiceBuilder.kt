56c4529a2404ea8f92e5b8d4f410bdbad886ff97
fredgoum <alfredgoumou@gmail.com>
2021-07-21 20:56:14 +0200

    Consume web service and display temp on screen

---
+++ app/src/main/java/com/example/weather_group1_android/network/services/WeatherServiceBuilder.kt
@@ -0,0 +1,59 @@
+/**
+ * This class allows to build our services with retrofit and interface.
+ * */
+package com.example.weather_group1_android.network.services
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import com.example.weather_group1_android.network.models.WeatherAsset
+import okhttp3.OkHttpClient
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class WeatherServiceBuilder {
+    companion object {
+        const val baseUrl = "https://api.openweathermap.org/"
+        const val q = "Paris,fr"
+        const val lang = "fr"
+        const val units = "metric"
+        const val AppId  = "e3e53a88fe0338f43a52da7aee06e41c"
+    }
+
+    /**
+     * Allows to check the network status
+     * */
+    fun isNetworkAvailable(context: Context) =
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).run {
+            getNetworkCapabilities(activeNetwork)?.run {
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            } ?: false
+        }
+
+    /**
+     * Configure and return and retrofit instance
+     * */
+    private fun getRetrofit(): Retrofit {
+        val okBuilder = OkHttpClient().newBuilder().apply {
+            connectTimeout(60, TimeUnit.SECONDS)
+            callTimeout(60, TimeUnit.SECONDS)
+            readTimeout(60, TimeUnit.SECONDS)
+            writeTimeout(60, TimeUnit.SECONDS)
+        }
+        return  Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okBuilder.build())
+            .build()
+    }
+
+    /**
+     * Allows the service to be consumed asynchronously
+     * */
+    suspend fun getAssets(): Response<WeatherAsset> = getRetrofit().create(WeatherService::class.java)
+        .assets(q, lang, units, AppId)
+}
---
+++ app/src/main/java/com/example/weather_group1_android/network/ui/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.weather_group1_android.network.ui
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.weather_group1_android.databinding.ActivityMainBinding
+import com.example.weather_group1_android.network.models.WeatherAsset
+import com.example.weather_group1_android.network.services.WeatherServiceBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val WeatherService by lazy {
+        WeatherServiceBuilder()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadAssets()
+    }
+
+    private fun loadAssets() {
+        if(!WeatherService.isNetworkAvailable(this)) {
+            displayError()
+            return
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = WeatherService.getAssets()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    displayResults(response.body())
+                } else {
+                    displayError()
+                }
+            }
+        }
+    }
+    private fun displayResults(response: WeatherAsset?) {
+        response?.let {
+            binding.temp.text = response.list[0].main.temp.toString() + "°C"
+        }
+    }
+    private fun displayError() {
+        Toast.makeText(this, "Error loading assets from CoinCap API", Toast.LENGTH_LONG).show()
+    }
+}
