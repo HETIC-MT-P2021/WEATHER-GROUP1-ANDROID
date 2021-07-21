f33756f4d355a8a617aa5eb5679f7561e1dba8a0
fredgoum <alfredgoumou@gmail.com>
2021-07-21 20:51:02 +0200

    Define the interface for web service consumption

---
+++ app/src/main/java/com/example/weather_group1_android/network/services/WeatherService.kt
@@ -0,0 +1,21 @@
+/**
+ * It is an interface that contains the assets() function,
+ * which returns the API data using a retrofit call function.
+ * */
+package com.example.weather_group1_android.network.services
+
+import com.example.weather_group1_android.network.models.WeatherAsset
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface WeatherService {
+    @Headers("Content-type: application/json")
+    @GET("data/2.5/forecast?")
+    suspend fun assets(@Query("q") q: String,
+                       @Query("lang") lang: String,
+                       @Query("units") units: String,
+                       @Query("APPID") app_id: String)
+    : Response<WeatherAsset>
+}
